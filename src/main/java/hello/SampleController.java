ef230c0e974d58b969f72e6b8435c19e68dffb3d
gejunwen <junwen.ge@creditcloud.com>
2015-06-19 14:24:15 +0800

    Test for validation

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -1,10 +1,17 @@
 package hello;
 
+import lombok.Data;
+import lombok.ToString;
+import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
 
 /**
  * Created by Junwen on 6/16/15.
@@ -21,5 +28,20 @@ public class SampleController {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SampleController.class, args);
+    }
+
+    @RequestMapping("/validation")
+    @ResponseBody
+    String withValidation(@Valid Input input) {
+        System.out.println(input);
+        return "Success";
+    }
+
+    @Data
+    @ToString
+    private static class Input {
+        int inputId;
+        @NotBlank
+        String inputValue;
     }
 }
